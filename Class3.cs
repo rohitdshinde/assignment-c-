83295fbda85aa2826883247db5c58f1654b2f2ac
rohitdshinde <157791747+rohitdshinde@users.noreply.github.com>
2024-02-08 02:11:10 +0530

    Add files via upload
    12 questions in file

--- Class3.cs
+++ Class3.cs
@@ -8,42 +8,129 @@ namespace ConsoleApp1
 {
     internal class Class3
     {
-        class emp {
-            int emp_id;
-            string emp_name;
-            string emp_depart;
-            int emp_exp;
-            int emp_salary;
+        void Q1()
+        {
+            Console.WriteLine("Enter a number");
+            int n = Convert.ToInt32(Console.ReadLine());
 
-            public emp(int emp_id,string emp_name,string emp_depart, int emp_exp,int emp_salary)
+            if (n%2 == 0 ) 
             {
-                this.emp_id = emp_id;
-                this.emp_name = emp_name;
-                this.emp_depart = emp_depart;
-                this.emp_exp = emp_exp;
-                this.emp_salary= emp_salary;
+                Console.WriteLine("Number is even");
             }
-
-            public string ToString()
+            else if (n % 2 == 1)
             {
-                return  emp_name;
-                   
-
-              /*  Console.WriteLine("id:"+emp_id);
-                Console.WriteLine("Name:" + emp_name);
-                Console.WriteLine("depart" + emp_depart);
-                Console.WriteLine("exp" + emp_exp);
-                Console.WriteLine("salary" + emp_salary);*/
-
+                Console.WriteLine("Number is odd");
             }
         }
+
+        void Q2()
+        {
+            int[] arr = {5,8,4,6,1,25};
+            /* (int i in arr)
+            {
+                if(i%5 == 0) { Console.WriteLine(arr[i]); }
+            }*/
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    Console.WriteLine($"Number is divisable by index {i} is {arr[i]}");
+                }
+            }
+            
+        }
+        void Q3()
+        {
+            int fact=1;
+            Console.WriteLine("Enter a number");
+            int n = Convert.ToInt32(Console.ReadLine());
+           
+            for(int i = 1;i <=n;i++)
+            {
+                fact = fact * i;
+            }
+           
+            Console.WriteLine(fact);
+        }
+        void Q4()
+        {
+            int[] a = new int[5];
+            Console.WriteLine("Enter a number");
+            for (int i = 0;i <a.Length; i++)
+            {
+                a[i] = Convert.ToInt32(Console.ReadLine()); 
+            }
+            foreach(int k in a)
+            {
+                if (k < 10)
+                {
+                    Console.WriteLine("number is less than 10 is" +k);
+                }
+            }
+        }
+        void Q5()
+        {
+            int count = 0;
+            Console.WriteLine("Enter a number");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    count++;
+                   
+                }
+               
+            }
+            if (count == 2)
+            {
+                Console.WriteLine("Prime");
+
+            }
+            else
+            {
+                Console.WriteLine("Not Prime");
+            }
+        }
+        void Q6()
+        {
+           
+            
+            Console.WriteLine("Enter a number");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int tmp = n, sum = 0; 
+
+            while (tmp != 0) {
+               
+               int digit=tmp%10;
+                sum = sum * 10 + digit;
+                tmp = tmp / 10;
+
+               
+            }
+            if (n == sum)
+            {
+                Console.WriteLine("Paildrome YES");
+            }
+            else
+            {
+                Console.WriteLine("Paildrome Not");
+            }
+        }
+
+
         static void Main(string[] args)
         {
-            emp p = new emp(1,"Rohit","bcs",2,350000);
-            // p.display();
-            Console.WriteLine(p.ToString());
+            Class3 n= new Class3();
+            //n.Q1();
+            //n.Q2();
+            // n.Q3();
+            //n.Q4();
+            // n.Q5();
+           // n.Q6();
 
-            
+            Console.ReadKey();
         }
     }
 }
