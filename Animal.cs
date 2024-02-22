85f803027a61b01aa83d59d72ea78a3428bb61a9
rohitdshinde <157791747+rohitdshinde@users.noreply.github.com>
2024-02-22 16:24:28 +0530

    Add files via upload
    Using parent and child relation (Inheritance)

---
+++ Animal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Animal
+    {
+        public virtual void sound()
+        {
+            Console.WriteLine("I am Animal");
+        }
+    }
+    class cat : Animal
+    {
+        public override void sound()
+        {
+            Console.WriteLine("I am not Animal");
+        }      
+         public static void Main(string[] args)
+        {
+            cat c = new cat();
+            c.sound();
+        }
+    }
+}
